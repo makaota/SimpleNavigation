9fb33080d851484f4fec3766e865216629d4022b
makaota <74915165+makaota@users.noreply.github.com>
2022-06-24 13:18:58 +0200

    Initial commit

---
+++ app/src/main/java/makaota/app/simplenavigation/MainActivity.kt
@@ -0,0 +1,44 @@
+package makaota.app.simplenavigation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import makaota.app.simplenavigation.ui.theme.SimpleNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Button(onClick = {
+                    val navigate = Intent(this@MainActivity, SecondActivity::class.java)
+                    startActivity(navigate)
+                    
+                }) {
+                    Text(text = "Navigate", fontSize = 18.sp)
+                    
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/makaota/app/simplenavigation/SecondActivity.kt
@@ -0,0 +1,34 @@
+package makaota.app.simplenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import makaota.app.simplenavigation.ui.theme.SimpleNavigationTheme
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "Welcome to Second Screen", fontSize = 30.sp)
+            }
+
+        }
+    }
+}
